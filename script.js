25777a2d0eb9bb78ed1bcfeb1a5225fd1cc57b0a
Salihu Muhammad <salihuumuhammad@gmail.com>
2026-04-09 21:42:49 +0200

    first lesson: Variables + Datatypes

---
+++ script.js
@@ -0,0 +1,23 @@
+const name = "Muhammad"
+const email = "PrimeDev"
+const department = "Business Administration"
+const faculty = "Management Science"
+const hasResumedSecondSemester = false
+const cGPA = 4.60
+const isLearning = true
+const inHostel = false
+const level = 100
+const resumptionDate = "April 15"
+
+console.log({
+    name,
+    email,
+    department,
+    faculty,
+    hasResumedSecondSemester,
+    cGPA,
+    isLearning,
+    inHostel,
+    level,
+    resumptionDate
+})
