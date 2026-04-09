b01a72ab15c2a87b7edf31bfa3c2eb94088f2c31
Salihu Muhammad <salihuumuhammad@gmail.com>
2026-04-09 23:13:58 +0200

    second lesson: Conditionals and logical operators

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-const name = "Muhammad"
-const email = "PrimeDev"
-const department = "Business Administration"
-const faculty = "Management Science"
-const hasResumedSecondSemester = false
-const cGPA = 4.60
-const isLearning = true
-const inHostel = false
-const level = 100
-const resumptionDate = "April 15"
+//MATH OPERATORS
 
-console.log({
-    name,
-    email,
-    department,
-    faculty,
-    hasResumedSecondSemester,
-    cGPA,
-    isLearning,
-    inHostel,
-    level,
-    resumptionDate
-})
+/* let score = 492
+console.log(score + 3 )
+console.log(score - 5)
+console.log(score * 2)
+console.log(score / 2)
+ */
+
+//CONDITIONALS or LOGICAL OPERATORS
+const age = 3
+if (3 < age) {
+    console.log("Free")
+} else if (12 < age) {
+    console.log("$5")
+} else if (17 < age) {
+    console.log("$7")
+} else if (64 < age) {
+    console.log("$10")
+} else {
+    console.log("$6")
+}
+
+const isLoggedin = true
+const hasCard = true
+if (isLoggedin && hasCard) {
+     console.log("Allow") 
+} else if (isLoggedin || hasCard) {
+    console.log("Pass but warn: At Least one must be correct")
+} else if (!isLoggedin && !hasCard) {
+    console.log("yes")
+}
