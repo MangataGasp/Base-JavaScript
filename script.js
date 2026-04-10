1d7e4251bee102662d43b041f0dfdf4b065596f6
Salihu Muhammad <salihuumuhammad@gmail.com>
2026-04-10 23:13:08 +0200

    third lesson: learnt about functions, was real busy thats why i didnt just move fast

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-//MATH OPERATORS
+/*  function numbers(prc1, prc2) {
+    return prc1 +  prc2;
+} 
 
-/* let score = 492
-console.log(score + 3 )
-console.log(score - 5)
-console.log(score * 2)
-console.log(score / 2)
- */
+let result = numbers(9, 5)
 
-//CONDITIONALS or LOGICAL OPERATORS
-const age = 3
-if (3 < age) {
-    console.log("Free")
-} else if (12 < age) {
-    console.log("$5")
-} else if (17 < age) {
-    console.log("$7")
-} else if (64 < age) {
-    console.log("$10")
-} else {
-    console.log("$6")
+console.log(result) */ 
+
+function student(score) {
+    if(score >= 80) {
+        return "Excelent"
+    } else if (score <= 79) {
+        return "Pass"
+    } else if (score <= 49) {
+        return "Fail"
+    }
 }
-
-const isLoggedin = true
-const hasCard = true
-if (isLoggedin && hasCard) {
-     console.log("Allow") 
-} else if (isLoggedin || hasCard) {
-    console.log("Pass but warn: At Least one must be correct")
-} else if (!isLoggedin && !hasCard) {
-    console.log("yes")
-}
+console.log(student(60))
